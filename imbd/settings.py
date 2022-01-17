844d186be2b22de91cbe1f3e15d823c91fd6dbb3
ggroshansii <garthgroshansii@gmail.com>
2022-01-16 23:06:21 -0500

    added django rest frameworks to INSTALLED_APPS

--- imbd/settings.py
+++ imbd/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps
     'watchlist.apps.WatchlistConfig'
+    
+    #thirdparty
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
