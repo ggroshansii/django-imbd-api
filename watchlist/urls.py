c879d0ed8d1b8b1d70d488b0c29c1b331d9b131a
ggroshansii <garthgroshansii@gmail.com>
2022-01-17 14:50:21 -0500

    created serializers.py in api app

--- watchlist/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from watchlist.views import movie_list, movie_detail
-
-urlpatterns = [
-    path('list/', movie_list, name="movie_list" ),
-    path('<int:pk>', movie_detail, name="movie_detail")
-]
