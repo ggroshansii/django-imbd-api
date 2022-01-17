1b7235a93c533ed8f0abc921c8cad62167fb21d9
ggroshansii <garthgroshansii@gmail.com>
2022-01-17 14:44:11 -0500

    transferred watchlist urls to api urls; revised imbd urls.py

--- imbd/urls.py
+++ imbd/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('watchlist.urls')),
+    path('movie/', include('api.urls')),
 ]
