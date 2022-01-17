b3cb6c06bfc0aa3812a012ed6dfc7e2dd3960451
ggroshansii <garthgroshansii@gmail.com>
2022-01-17 14:56:15 -0500

    created MovieSerializer based on Movie model

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
-from watchlist.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist.models import Movie
+# from django.http import JsonResponse
 
-# Create your views here.
+# # Create your views here.
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {'movies': list(movies.values())}
-    return JsonResponse(data)
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {'movies': list(movies.values())}
+#     return JsonResponse(data)
 
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {'id': movie.id, 'name': movie.name, 'description': movie.description}
-    return JsonResponse(data)
+# def movie_detail(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {'id': movie.id, 'name': movie.name, 'description': movie.description}
+#     return JsonResponse(data)
